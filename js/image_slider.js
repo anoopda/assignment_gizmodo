2760095387be74cca1664b039617c685d932ea6a
anoopda <anoopda@qburst.com>
2016-09-06 17:21:06 +0530

    completed content

---
+++ js/image_slider.js
@@ -0,0 +1,23 @@
+
+$( "#concorse" ).mouseenter(function() {
+	$(".wrapper_none").hide();
+	$( "#concorse2" ).show();
+});
+
+$( "#garage" ).mouseenter(function() {
+	$(".wrapper_none").hide();
+
+	$( "#garage2" ).show();
+});
+
+
+$( "#cosplay" ).mouseenter(function() {
+	$(".wrapper_none").hide();
+
+	$( "#cosplay2" ).show();
+});
+
+
+
+
+
